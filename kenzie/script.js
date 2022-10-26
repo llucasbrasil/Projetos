3cbd4fb70aab5569e08bad77493ba881657277ea
Lucas Brasileiro <97102750+llucasbrasil@users.noreply.github.com>
2022-10-25 23:20:42 -0300

    organizando

---
+++ kenzie/script.js
@@ -0,0 +1,40 @@
+
+const buyButton = document.getElementById('buy-button')
+const cartForm = document.getElementById('cart-form')
+
+const addTicket = document.getElementById('add-ticket')
+const removeTicket = document.getElementById('remove-ticket')
+const ticket = document.getElementById('ticket')
+const ticketPrice = document.getElementById('ticket-price')
+
+addTicket.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    if(parseInt(ticket.innerText) !== 5){
+        ticket.innerText = parseInt(ticket.innerText)+1
+        ticketPrice.innerText = parseInt(ticket.innerText)*90
+    }
+})
+
+removeTicket.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    if(parseInt(ticket.innerText) !== 1){
+        ticket.innerText = parseInt(ticket.innerText)-1
+        ticketPrice.innerText = parseInt(ticket.innerText)*90
+    }
+})
+
+cartForm.addEventListener("submit",(e) => {
+    e.preventDefault();
+    const nome = cartForm.inome.value;
+    const email = cartForm.iemail.value;
+    const quantityTickets = ticket.innerText;
+
+    window.localStorage.setItem('nome', nome)
+    window.localStorage.setItem('email', email)
+    window.localStorage.setItem('quantityTickets', quantityTickets)
+    window.location.href = "finalizacao.html"
+
+})
+
